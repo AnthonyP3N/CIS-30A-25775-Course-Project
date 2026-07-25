8989532a055f04148e7d281cefe977da86f62860
AnthonyP3N <apenaloza1@cpp.edu>
2026-07-25 12:05:39 -0700

    Add class file, add date feature

--- main.py
+++ main.py
@@ -1,20 +1,36 @@
 import tkinter as tk
+import pandas as pd
 
 # Custom mod file call
 import budget_math 
 
-window = tk.Tk()
-window.geometry("400x400")
+try:
+    df = pd.read_csv("./records.csv")
+except Exception as e:
+    data = {
+        "Date": [],
+        "Description":[],
+        "Amount":[],
+        "Type":[]
+    }
 
-label = tk.Label(
-    text="Budget App v0.0",
-    fg="white",
-    bg="black",
-    width= 30,
-    height=15
-)
+    df = pd.DataFrame(data)
 
-label.pack()
+# main class - Budget Tracker
+class BudgetTrackerApp:
+    def __init__(self, root):
+        # Initializes root and titlefor window
+        self.root = root
+        self.root.title("Budget Tracker")
 
-window.mainloop()
+        # Initializes label for window and text entry
+        self.label_date = tk.Label(root, text = "Date: [YY-MM-DD]")
+        self.label_date.grid(row = 0, column = 0)
+        self.entry_date = tk.Entry(root)
+        self.entry_date.grid(row = 0, column = 1)
+
+
+root = tk.Tk()
+app = BudgetTrackerApp(root)
+root.mainloop()
 
