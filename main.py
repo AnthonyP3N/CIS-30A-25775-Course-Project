863328a1f10a04e61fc99cb5a4e7224732914337
AnthonyP3N <apenaloza1@cpp.edu>
2026-07-25 13:28:14 -0700

    Add income button, window, and logic. Also made it so user can only have one expense and one income window open at a time

--- main.py
+++ main.py
@@ -31,6 +31,9 @@ class BudgetTrackerApp(MainMenu):
     def __init__(self, root):
         super().__init__(root, "Budget Tracker - Main Menu", "300x150")
 
+        # Create a variable to track the window state
+        self.expense_window = None
+        self.income_window = None
 
         option_label = tk.Label(root, text = "Select an Option: ")
         option_label.pack()
@@ -41,30 +44,80 @@ class BudgetTrackerApp(MainMenu):
                                    command= self.open_expense)
         expense_screen.pack()
 
+        income_screen = tk.Button(root, 
+                                   text="Add Income",
+                                   width = 15,
+                                   command= self.open_income)
+        income_screen.pack()
+
     # function to open new window for expense
     def open_expense(self):
-        expense_window = tk.Toplevel(self.root)
-        Expense(expense_window)
+        # Check if window is NONE of if it has been destroyed
+        # In place to prevent user from opening multiple expense/income windows
+        if self.expense_window is None or not self.expense_window.winfo_exists():
+            self.expense_window = tk.Toplevel(self.root)
+            Expense(self.expense_window)
+        else:
+            # If window already exists, bring it toward the front for user to use
+            self.expense_window.lift()
+
+    def open_income(self):
+        # Check if window is NONE of if it has been destroyed
+        # In place to prevent user from opening multiple income/expense windows
+        if self.income_window is None or not self.income_window.winfo_exists():
+            self.income_window = tk.Toplevel(self.root)
+            Income(self.income_window)
+        else:
+            # If window already exists, bring it toward the front for user to use
+            self.income_window.lift()
 
 # Expense Window - allow user to input information about a new expense 
 class Expense(MainMenu):
     def __init__(self, root):
         super().__init__(root, "Add Expense")
-        # Initializes label for window and text entry
+
+        # Initializes date label for window and date text entry
         self.label_date = tk.Label(root, text = "Date: [YY-MM-DD]")
         self.label_date.grid(row = 0, column = 0)
         self.entry_date = tk.Entry(root)
         self.entry_date.grid(row = 0, column = 1)
 
+        # Initializes expense label for window and expense text entry
         self.label_expense = tk.Label(root, text = "Expense: $")
         self.label_expense.grid(row = 1, column = 0)
         self.entry_expense = tk.Entry(root)
         self.entry_expense.grid(row = 1, column = 1)
 
+        # Initializes description label for window and description text entry
         self.label_description = tk.Label(root, text = "Description: ")
         self.label_description.grid(row = 2, column = 0)
         self.entry_description = tk.Entry(root)
         self.entry_description.grid(row = 2, column = 1)       
+
+
+# Income Window - allow user to input information about a new expense 
+class Income(MainMenu):
+    def __init__(self, root):
+        super().__init__(root, "Add Income")
+
+        # Initializes date label for window and date text entry
+        self.label_date = tk.Label(root, text = "Date: [YY-MM-DD]")
+        self.label_date.grid(row = 0, column = 0)
+        self.entry_date = tk.Entry(root)
+        self.entry_date.grid(row = 0, column = 1)
+
+        # Initializes expense label for window and income text entry
+        self.label_income = tk.Label(root, text = "Income: $")
+        self.label_income.grid(row = 1, column = 0)
+        self.entry_income = tk.Entry(root)
+        self.entry_income.grid(row = 1, column = 1)
+
+        # Initializes description label for window and description text entry
+        self.label_description = tk.Label(root, text = "Description: ")
+        self.label_description.grid(row = 2, column = 0)
+        self.entry_description = tk.Entry(root)
+        self.entry_description.grid(row = 2, column = 1)
+
 
 if __name__ == "__main__":
     root = tk.Tk()
